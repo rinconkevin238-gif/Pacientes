80fb5baa415606dea91e567f287618f5ae631584
rinconkevin238 <rinconkevin238@gmail.com>
2025-11-06 20:27:19 -0500

    Initial commit

---
+++ src/main/java/com/pacientes/api/PacientesApplication.java
@@ -0,0 +1,13 @@
+package com.pacientes.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacientesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PacientesApplication.class, args);
+    }
+
+}
